a789335ec40f5086a4b9770e4cab4f3dd01659fb
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-22 17:03:16 +0200

    set up DB connection with .env

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+
+// get environment variables from .env
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3307
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to Database:', err.stack);
+        return;
+    }
+    console.log('Connected to Database successfully');
+});
+
+export default db;
