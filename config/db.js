784916e9c0687f975e0f94c53949a20f14e81c67
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-23 14:51:04 +0200

    fix Sequelize connection and add User model

--- config/db.js
+++ config/db.js
@@ -1,23 +1,33 @@
-import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
 // get environment variables from .env
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = new Sequelize({
+    dialect: 'mysql',
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3307
+    port: process.env.DB_PORT || 3306
 });
 
-db.connect((err) => {
+db.authenticate()
+    .then(() => {
+        console.log('DB connection has been established successfully');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+/* db.connect((err) => {
     if (err) {
         console.error('Error connecting to Database:', err.stack);
         return;
     }
     console.log('Connected to Database successfully');
-});
+}); */
 
 export default db;
---
+++ models/User.js
@@ -0,0 +1,58 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db.js';
+
+const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    }, 
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    }, 
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, 
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    cover_image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    profile_image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    sex: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    }, 
+    weight: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+    }, 
+    height: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+    }, 
+}, {
+    // specify the name of the table that already exists in the database    
+    tableName: 'users',
+    timestamps: false,
+});
+
+export default User;
