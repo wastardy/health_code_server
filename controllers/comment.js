09690e3004704ad57931ff019b5ed917a7807d3a
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-25 22:33:45 +0200

    add logic for adding comments with token validation

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,4 +1,4 @@
-import { User, Comments } from '../models/associations.js';
+import { User, Comments, Posts } from '../models/associations.js';
 import serverErrorHandler from '../middleware/serverErrorHandler.js';
 import jwt from 'jsonwebtoken';
 import moment from 'moment';
@@ -55,7 +55,48 @@ export const getComments = async (req, res) => {
 }
 
 export const addComment = async (req, res) => {
+    const token = req.cookies.accessToken;
 
+    console.log(`\nToken from cookies: ${token}\n`);
+
+    if (!token) {
+        return res.status(401).json('User not logged in');
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, async (err, userInfo) => {
+        if (err) {
+            return res.status(403).json('Token is not valid')
+        }
+
+        const { desc, post_id } = req.body;
+
+        if (!desc || !post_id) {
+            return res.status(400).json('Description and Post ID are required');
+        }
+
+        const post = await Posts.findByPk(post_id);
+
+        if (!post) {
+            return res.status(404).json('Post not found');
+        }
+
+        try {
+            const newComment = await Comments.create({
+                description: desc,
+                created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+                user_id: userInfo.id,
+                post_id: post_id,
+            });
+
+            return res.status(200).json({
+                message: 'Comment has been created',
+                comment: newComment,
+            });
+        }
+        catch (err) {
+            serverErrorHandler(addComment.name, err, res);
+        }
+    });
 }
 
 export const deleteComment = async (req, res) => {
