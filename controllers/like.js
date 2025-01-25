e223180a26154397b875fe64912b8a4d5f4714eb
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-25 16:13:21 +0200

    add get likes logic

--- controllers/like.js
+++ controllers/like.js
@@ -1,8 +1,27 @@
 import Likes from '../models/Likes.js';
+import serverErrorHandler from '../middleware/serverErrorHandler.js';
 import jwt from 'jsonwebtoken';
 
 export const getLikes = async (req, res) => {
+    const { post_id } = req.query;
 
+    try {
+        const likes = await Likes.findAll({
+            where: { post_id: post_id },
+            attributes: ['user_id']
+        });
+
+        const userIds = likes.map(like => like.user_id);
+        const countLikes = userIds.length;
+
+        return res.status(200).json({
+            count_likes: countLikes,
+            user_ids: userIds,
+        });
+    }
+    catch (err) {
+        serverErrorHandler(getLikes.name, err, res);
+    }
 }
 
 export const addLike = async (req, res) => {
