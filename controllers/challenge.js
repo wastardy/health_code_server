b4c534ff6e95c2c8824dc5be6e28358e0ce23fd4
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-26 20:13:53 +0200

    add logic to create challenge

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -2,6 +2,7 @@ import { Op } from 'sequelize';
 import Challenges from '../models/Challenges.js';
 import Relationships from '../models/Relationships.js';
 import serverErrorHandler from '../middleware/serverErrorHandler.js';
+import moment from 'moment';
 
 export const getChallenges = async (req, res) => {
     const userId = req.user.id;
@@ -51,7 +52,58 @@ export const getChallenges = async (req, res) => {
 }
 
 export const addChallenge = async (req, res) => {
+    const userId = req.user.id;
+    const { type, title, description, start_date, end_date } = req.body;
 
+    try {
+        if (!type || !title || !description) {
+            return res.status(400).json(
+                'Type, title, and description are required.'
+            );
+        }
+
+        // chech type
+        const validTypes = [
+            'outdoor_activity', 
+            'indoor_activity', 
+            'sleep', 
+            'water', 
+            'nutrition'
+        ];
+
+        if (!validTypes.includes(type)) {
+            return res.status(400).json('Invalid type provided.');
+        }
+
+        // check date
+        const currentDate = new Date();
+        if (start_date && end_date && new Date(end_date) < new Date(start_date)) {
+            res.status(400).json('End date cannot be earlier than start date.');
+        }
+
+        const status = start_date && new Date(start_date) > currentDate
+            ? 'unavailable'
+            : 'active';
+
+        const newChallenge = await Challenges.create({
+            type,
+            title, 
+            description, 
+            start_date: start_date || null, 
+            end_date: end_date || null,
+            status,
+            created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+            user_id: userId,
+        });
+
+        return res.status(200).json({
+            message: 'Challenge added successfully!',
+            challenge: newChallenge,
+        });
+    }
+    catch (err) {
+        serverErrorHandler(addChallenge.name, err, res);
+    }
 }
 
 export const deleteChallenge = async (req, res) => {
