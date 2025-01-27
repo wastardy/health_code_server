d52c89bb8f2e5558550fba6e50c5abf2e4dbcd34
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-28 01:24:27 +0200

    add get relationships logic

--- controllers/relationship.js
+++ controllers/relationship.js
@@ -1,8 +1,31 @@
+import serverErrorHandler from '../middleware/serverErrorHandler.js';
 import Relationships from '../models/Relationships.js';
-import jwt from 'jsonwebtoken';
 
 export const getRelationships = async (req, res) => {
-    
+    try {
+        const userId = req.query.user_id;
+
+        if (!userId) {
+            return res.status(400).json('user_id is required');
+        }
+
+        // get all followers for user
+        const relationships = await Relationships.findAll({
+            where: {
+                follower_id: userId,
+            }
+        });
+
+        const followers = relationships.map(rel => rel.following_id);
+
+        return res.status(200).json({
+            followers_count: followers.length, 
+            followers: followers,
+        });
+    }
+    catch (err) {
+        serverErrorHandler(getRelationships.name, err, res);
+    }
 }
 
 export const addRelationship = async (req, res) => {
