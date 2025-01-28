82ef4bdc40ccbefb12ea158f6237b37e7936fc92
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-28 17:10:52 +0200

    add updateUser logic for updating user profile information

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
+import serverErrorHandler from '../middleware/serverErrorHandler.js';
 import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 export const getUser = async (req, res) => {
     const user_id = parseInt(req.params.user_id);
@@ -24,6 +25,58 @@ export const getUser = async (req, res) => {
     }
 }
 
-export const updateUser = (req, res) => {
+export const updateUser = async (req, res) => {
+    try {
+        const currentUserId = parseInt(req.params.user_id);
+        const userInfo = req.user;
 
+        console.log(`\nCurrent user id: ${JSON.stringify(currentUserId)}\n`);
+        console.log(`\nUser info: ${JSON.stringify(userInfo)}\n`);
+        
+        if (userInfo.id !== currentUserId) {
+            return res.status(403).json('User can only update his own profile!');
+        }
+
+        // hash password if it's been passed to body
+        if (req.body.password) {
+            // const salt = await bcrypt.genSalt(10);
+            // const hashedPassword = await bcrypt.hash(password, salt);
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+        }
+
+        const [updated] = await User.update(
+            {
+                username: req.body.username, 
+                email: req.body.email, 
+                password: req.body.password,
+                name: req.body.name,
+                cover_image: req.body.cover_image,
+                profile_image: req.body.profile_image, 
+                bio: req.body.bio, 
+                sex: req.body.sex,
+                age: req.body.age,
+                weight: req.body.weight,
+                height: req.body.height,
+            }, 
+            {
+                where: { id: currentUserId },
+            },
+        );
+
+        if (updated) {
+            const updatedUser = await User.findByPk(currentUserId, {
+                attributes: { exclude: ['password'] }
+            });
+
+            return res.status(200).json({
+                message: 'User profile has been updated',
+                updated_user_info: updatedUser,
+            });
+        }
+
+        return res.status(404).json('User not found');
+    }
+    catch (err) {
+        serverErrorHandler(updateUser.name, err, res);
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,14 @@
 import express from 'express';
-import { getUser, updateUser } from '../controllers/user.js';
+import verifyToken from '../middleware/checkAuth.js';
+import { 
+    getUser, 
+    updateUser 
+} from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get('/:user_id', (req, res, next) => {
-    console.log(`Received GET request for user_id: ${req.params.user_id}`);
-    next();
-}, getUser);
+router.get('/:user_id', verifyToken, getUser);
 
-router.put('/:user_id', (req, res, next) => {
-    console.log(`Received PUT request for user_id: ${req.params.user_id}`);
-    next();
-}, updateUser);
+router.put('/:user_id', verifyToken, updateUser);
 
 export default router;
